b78344a2c3564308598aaec23d57b9358964fa31
shahin-trunk <shahin.konadath@outlook.com>
2023-11-15 13:35:43 +0400

    Bug fix: Added "gen_cleaners" in text cleaners.

--- TTS/tts/datasets/dataset.py
+++ TTS/tts/datasets/dataset.py
@@ -317,17 +317,18 @@ class TTSDataset(Dataset):
             item["audio_length"] = audio_length
             item["text_length"] = text_length
             new_samples += [item]
-        print(f"new_samples[-1]: {new_samples[-1]}")
+        # print(f"new_samples[-1]: {new_samples[-1]}")
         return new_samples
 
     @staticmethod
     def filter_by_length(lengths: List[int], min_len: int, max_len: int):
         idxs = np.argsort(lengths)  # ascending order
-        print(f"idxs: {idxs}")
+        # print(f"idxs: {idxs}")
         ignore_idx = []
         keep_idx = []
         for idx in idxs:
             length = lengths[idx]
+            print(f"idx: {idx}, length: {length}, min_len: {min_len}, max_len: {max_len}")
             if length < min_len or length > max_len:
                 ignore_idx.append(idx)
             else:
@@ -369,7 +370,6 @@ class TTSDataset(Dataset):
         audio_lengths = [i["audio_length"] for i in samples]
         text_ignore_idx, text_keep_idx = self.filter_by_length(text_lengths, self.min_text_len, self.max_text_len)
         audio_ignore_idx, audio_keep_idx = self.filter_by_length(audio_lengths, self.min_audio_len, self.max_audio_len)
-        print
         keep_idx = list(set(audio_keep_idx) & set(text_keep_idx))
         ignore_idx = list(set(audio_ignore_idx) | set(text_ignore_idx))
 
